161f7550b36629d996d9d920f08ee953ed43b6cb
Franziska <franziska.boenisch.fb@gmail.com>
2018-10-05 14:21:14 +0100

    cleaned code

--- visualization/drawTrajectoriesOnVideo.m
+++ visualization/drawTrajectoriesOnVideo.m
@@ -25,66 +25,61 @@ UD.T                = Params.T;     %trajectory
 Q                   = cat(1, Params.T{:});
 UD.idx              = min(Q(:, 1)) : max(Q(:, 1));  % find the earliest start index in all tracks
 UD.img              = read(UD.vidobj, UD.idx(1));   % earliest frame where sth is tracked
-UD.colors           = 'ymbgrymbgrrgbmy';            % color array for different tracks
-%UD.fig              = figure(1); 
+UD.colors           = 'ymbgrcymbgrc';               % color array for different tracks
 
-% initialize the start points of the lines
+% initialize the start points of the lines for the trajectories
+% and the bounding boxes
 for i = 1:length(Params.T)
-    UD.roi{i}(1,:)     = UD.T{i}(1, 2:6);     % contains data of track start
-    UD.lineHandle{i}   = [0 0 0]; %?? was macht das?
-
-    % is for the current frame data is available, plot it
-    %if ( UD.roi{i}(1,1) == UD.idx(1) )
-    %    UD = plotROI(UD, i, 1, [ ':' UD.colors(i) ]); % plot ROI #1
-    %end
+    UD.roi{i}(1,:)     = UD.T{i}(1, 2:6);  
+    UD.box{i} = gobjects(0);
 end
 
 % iterate over all frames where we have tracked data
 for frameIdx = 1 : length(UD.idx)
-    UD.i = frameIdx;
-   % UD = readNewFrameAndDisplay(UD);
-    UD.img = read(UD.vidobj, UD.idx(UD.i));
-    hIm.CData = UD.img;
-    for p = 1:length(UD.T)
-        %index in UD.T where trajectory data is available for the current frame index
-        j{p} = find( UD.idx(UD.i) == UD.T{p}(:,1) );
-        
-        if ~isempty(j{p})        
-            j{p}
-            xOld = UD.roi{p}(1,1); % UD.roi has 5 values (not the frame index)
-            xPixel = UD.T{p}(j{p}, 2); % UD.T still has 6 values, first one being frame index
-            yOld = UD.roi{p}(1,2);
-            yPixel = UD.T{p}(j{p}, 3);
-        
    
-            plot(hAx, [xOld xPixel], [UD.vidobj.Height-yOld UD.vidobj.Height-yPixel], UD.colors(p));
-            UD.roi{p}(1,:)     = UD.T{p}(j{p}, 2:6)';
+    % display the current frame with correct title
+    UD.i = frameIdx;
+    UD.img = read(UD.vidobj, UD.idx(UD.i));
+    hFig.Name = printText(UD);
+    hIm.CData = UD.img;
+    
+    % iterate over all available trajectories
+    for p = 1:length(UD.T)
+        
+        % if trajectory has data for current frame index
+        j{p} = find( UD.idx(UD.i) == UD.T{p}(:,1) );
+        if ~isempty(j{p})        
             
+            % plot the lines following the trajectories
+            linePoints = getLinePoints(UD, j, p);
+            plot(hAx, linePoints(1,:), linePoints(2,:), UD.colors(p));
+                        
+            % TODO: do not hardcode 27x10 as box sizes; deal with flipped
+            % y-axis and thus flipped angle for robot; do not make it look
+            % overloaded
+            delete(UD.box{p})
+            UD.box{p} = plot_RotatedRectangle(linePoints(:,2)', -UD.T{p}(j{p},6),27,UD.colors(p),10);
+            
+            % update the roi to the current line in trajectory
+            UD.roi{p}(1,:)     = UD.T{p}(j{p}, 2:6)';
         end
     end
     F = getframe(hAx);
 end
 
-
-%UD.N                = length(UD.idx);
-%close all
-
-
-function printText(UD)
+% make the title for the graphic window
+function s = printText(UD)
 minutes = floor(UD.idx(UD.i) / (UD.vidobj.FrameRate * 60));
 seconds = floor(UD.idx(UD.i) / (UD.vidobj.FrameRate) - minutes * 60);
 
 s = sprintf('Showing frame %d / %d (%d:%d) - frameID:%d.', UD.i, length(UD.idx), minutes, seconds, UD.idx(UD.i));
-title(s);
 
-function UD = readNewFrameAndDisplay(UD)
-UD.img = read(UD.vidobj, UD.idx(UD.i));                 %get new image
-%figure(UD.fig);
-%hold off                                                %hold off to erase old data
-%imshow(UD.img);                                         %draw it
-% hold on                                                 %hold plot
-% printText(UD);                                          %draw information on figure
-%hIm.CData = UD.img;
-for i = 1:length(UD.T)
-    UD.lineHandle{i} = zeros(1,3);                             %delete all (now invalid) handles
-end
+% gets the start and end x/y-points for the lines/boxes
+function points = getLinePoints(UD, j, p)
+xOld = UD.roi{p}(1,1); % UD.roi has 5 values (not the frame index), thus x is first value, y second
+xPixel = UD.T{p}(j{p}, 2); % UD.T still has 6 values, first one being frame index
+yOld = UD.roi{p}(1,2);
+yPixel = UD.T{p}(j{p}, 3);
+% we need to subtract from height as video is flipped 
+points = [xOld xPixel; UD.vidobj.Height-yOld UD.vidobj.Height-yPixel];
+
