4b77fd15298d7315c094e3fe202599132f1874b8
Franziska <franziska.boenisch.fb@gmail.com>
2018-11-06 16:27:05 +0100

    corrected wrong understanding of where head of the robot is. now consistent with follower

--- visualization/plot_RotatedRectangle.m
+++ visualization/plot_RotatedRectangle.m
@@ -31,8 +31,8 @@ Y3      = YM2 + sinb;
 X4      = XM2 - cosb;
 Y4      = YM2 - sinb;
 
-% original one - also necessary for the follower :/
-% b = plot([X1 X2 X4 X3 X1 XM1 X3 XM1 X4]', [Y1 Y2 Y4 Y3 Y1 YM1 Y3 YM1 Y4]', s);
+% original one
+b = plot([X1 X2 X4 X3 X1 XM1 X3 XM1 X4]', [Y1 Y2 Y4 Y3 Y1 YM1 Y3 YM1 Y4]', s);
 
 % adapted to the fact that the image is flipped by y in our visualization 
-b = plot([X1 X2 X4 X3 X1 XM2 X1 XM2 X2]', [Y1 Y2 Y4 Y3 Y1 YM2 Y1 YM2 Y2]', s);
+%b = plot([X1 X2 X4 X3 X1 XM2 X1 XM2 X2]', [Y1 Y2 Y4 Y3 Y1 YM2 Y1 YM2 Y2]', s);
