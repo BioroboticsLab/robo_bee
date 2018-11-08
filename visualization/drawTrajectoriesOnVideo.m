0a0e0cd9e76d41b5c7c28de33811e07748900eae
Franziska <franziska.boenisch.fb@gmail.com>
2018-11-08 16:14:14 +0100

    add possibility to display trajectories for rectified videos

--- visualization/drawTrajectoriesOnVideo.m
+++ visualization/drawTrajectoriesOnVideo.m
@@ -22,24 +22,53 @@ UD.T                = Params.T;     %trajectory
 
 % Q holds all Trajectories, in order to find the indices in which data is
 % available
-Q                   = cat(1, Params.T{:});
+Q                   = cat(1, UD.T{:});
 UD.idx              = min(Q(:, 1)) : max(Q(:, 1));  % find the earliest start index in all tracks
 UD.img              = read(UD.vidobj, UD.idx(1));   % earliest frame where sth is tracked
 UD.colors           = 'ymbgrcymbgrc';               % color array for different tracks
+UD.ending           = Params.fileending;
+
+
+% for rectified images needed for the conversion
+if ~strcmp(UD.ending,'*.raw')
+    UD.Trans = Params.Trans; %transformation matrix
+    [width, height, xStartPixel, yStartPixel] = initializeWorldCoordsToPixel(UD);
+    UD.width = width;
+    UD.height = height;
+    UD.xStartPixel = xStartPixel;
+    UD.yStartPixel = yStartPixel;
+end
+
+
+% line handles to remove path segments
+storage = java.util.PriorityQueue(5);% matlab.DiscreteEventSystem.queueFIFO('handle', 20);
+
+UD = rectified_track_to_pixel(UD);
 
 % initialize the start points of the lines for the trajectories
 % and the bounding boxes
-for i = 1:length(Params.T)
+for i = 1:length(UD.T)
+   
     UD.roi{i}(1,:)     = UD.T{i}(1, 2:6);  
-    UD.box{i} = gobjects(0);
+    UD.box{i} = gobjects(0); % empty box
 end
 
+
+
+
+     
 % iterate over all frames where we have tracked data
 for frameIdx = 1 : length(UD.idx)
    
     % display the current frame with correct title
     UD.i = frameIdx;
     UD.img = read(UD.vidobj, UD.idx(UD.i));
+    
+    % for rectified video, we need to flip the image
+    if ~strcmp(UD.ending,'*.raw')
+        UD.img = flipud(UD.img);
+    end
+    
     hFig.Name = printText(UD);
     hIm.CData = UD.img;
     
@@ -52,12 +81,19 @@ for frameIdx = 1 : length(UD.idx)
             
             % plot the lines following the trajectories
             linePoints = getLinePoints(UD, j, p);
-            plot(hAx, linePoints(1,:), linePoints(2,:), UD.colors(p));
-                        
+            h = plot(hAx, linePoints(1,:), linePoints(2,:), UD.colors(p));
+            %storage.add(h);
+            
+            % never show more than 5 line segments
+            %if storage.size() >= 5
+             %   handle = storage.remove();
+                %disp(handle);
+                %delete(handle);
+            %end
+            
             % TODO: do not hardcode 27x10 as box sizes; deal with flipped
-            % y-axis and thus flipped angle for robot; do not make it look
-            % overloaded
             delete(UD.box{p})
+            %%%%%%%%%%%% TODO: RECTIFIED VERSION! angle
             UD.box{p} = plot_RotatedRectangle(linePoints(:,2)', -UD.T{p}(j{p},6),27,UD.colors(p),10);
             
             % update the roi to the current line in trajectory
@@ -65,7 +101,7 @@ for frameIdx = 1 : length(UD.idx)
         end
     end
     F = getframe(hAx);
-end
+end 
 
 % make the title for the graphic window
 function s = printText(UD)
@@ -74,12 +110,77 @@ seconds = floor(UD.idx(UD.i) / (UD.vidobj.FrameRate) - minutes * 60);
 
 s = sprintf('Showing frame %d / %d (%d:%d) - frameID:%d.', UD.i, length(UD.idx), minutes, seconds, UD.idx(UD.i));
 
+
+
 % gets the start and end x/y-points for the lines/boxes
+% UD: userdata contains all tracks
+% j: stores data from current frame index
+% p: idx of track (if there are several tracks in UD.T
 function points = getLinePoints(UD, j, p)
+% safe the old values to use them in the line
 xOld = UD.roi{p}(1,1); % UD.roi has 5 values (not the frame index), thus x is first value, y second
-xPixel = UD.T{p}(j{p}, 2); % UD.T still has 6 values, first one being frame index
 yOld = UD.roi{p}(1,2);
+
+
+xPixel = UD.T{p}(j{p}, 2); % UD.T still has 6 values, first one being frame index
 yPixel = UD.T{p}(j{p}, 3);
+    
+
 % we need to subtract from height as video is flipped 
 points = [xOld xPixel; UD.vidobj.Height-yOld UD.vidobj.Height-yPixel];
+
+% if the rectified or spline interpolated tracks are given, their world 
+% coordinates need to be converted to pixel coodinates for plotting.
+% udata: image width in pixel
+% vdata: image height in pixel
+function [width, height, xStartPixel, yStartPixel] = initializeWorldCoordsToPixel(UD)%v_in, Trans)
+    pic = UD.img; %reads the first frame
+
+    udata = [1 640];  
+    vdata = [1 480];
+
+    [~,xdata,ydata] = imtransform(pic,UD.Trans,'bicubic','udata',udata,...
+                                                    'vdata',vdata,...
+                                                    'size',size(pic),...
+                                                    'fill',128);
+
+    % xdata: leftmost and rightmost image coord in mm 
+    % (after rectification) e.g. [-30.0 157.8]  
+    % y data: similar for lowest and highest
+    width = xdata(2) - xdata(1); %width of frame in mm
+    height = ydata(2) - ydata(1); %height of frame in mm
+
+    xStartPixel = xdata(1)/width * UD.vidobj.Width; % leftmost image coord in pixel
+    yStartPixel = ydata(1)/height * UD.vidobj.Width; % most up image coord in pixel
+
+    
+% given a raw angle, display the rectified version of it
+% UD: userdata contains all tracks
+% j: stores data from current frame index
+% p: idx of track (if there are several tracks in UD.T
+function angle = convertAngleToRectified(UD, j, p)
+raw_angle = UD.T{p}(j{p}, 6)
+V   = [cos(raw_angle) sin(raw_angle)];
+
+angle = 0;
+
+% iterates over a rectified track and converts all mm-coordinates to pixel
+function UD = rectified_track_to_pixel(UD)
+Tracks = UD.T;
+
+% iterate over all tracks
+for i = 1:length(Tracks)
+    
+    % iterate over all rows in the track
+    for j = 1:length(Tracks{i})
+        % calculate the pixel
+        xPixel = Tracks{i}(j, 2)/ UD.width * UD.vidobj.Width - UD.xStartPixel;
+        yPixel = Tracks{i}(j, 3)/ UD.height * UD.vidobj.Height - UD.yStartPixel;
+        % and overwrite the milimeter data with pixel data
+        Tracks{i}(j, 2) = xPixel;
+        Tracks{i}(j, 3) = yPixel;
+    end
+
+    UD.T = Tracks;
+end
 
