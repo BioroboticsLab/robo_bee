dc360373d18534fe6e8319c0caf8550bc9b5e8e3
Franziska <franziska.boenisch.fb@gmail.com>
2018-11-21 12:17:09 +0100

    implemented logic to iterate over several robotic dances

--- analyses/analyzeHeadBodyDistancesAndAngles.m
+++ analyses/analyzeHeadBodyDistancesAndAngles.m
@@ -1,11 +1,32 @@
 % folder: leads to the upper folder of trajectories and video
+% e.g. C:\Users\Franzi\Downloads\Arbeitsergebnisse\tmp\robotic_dances\
 function analyzeHeadBodyDistancesAndAngles( folder )
 % analyzes the head body distances and angles of bees and followers
 
-Params = loadTrajectoryFilesFromFolder(folder, '*.ups');
+% list all subdirectories
+D = dir(folder);
+% Get a logical vector that tells which is a directory.
+dirFlags = [D.isdir];
+% Extract only those that are directories.
+D = D(dirFlags);
 
-[Pr, Pf, W] = getDancerAndFollowerTrajectorySyncd(Params);
-getMeanDistanceAndAngle_WaggleReturnSeperated(Pr, Pf, W, Params.framerate)
+% for each subdirectory (each following behavior), execute the analyses
+for i = 1:length(D)
+    
+    if strcmp(D(i).name, '.') || strcmp(D(i).name, '..') || strcmp(D(i).name(end), '_')
+        % the directories '.' and '..' won't be used as well as directories
+        % starting with an underscore (they do not have all data necessary)
+        
+        continue
+    end
+    
+    subfolder = strcat(folder, D(i).name);
+    Params = loadTrajectoryFilesFromFolder(subfolder, '*.ups');
+
+    [Pr, Pf, W] = getDancerAndFollowerTrajectorySyncd(Params);
+    getMeanDistanceAndAngle_WaggleReturnSeperated(Pr, Pf, W, Params.framerate)
+end
+
 
 end
 
