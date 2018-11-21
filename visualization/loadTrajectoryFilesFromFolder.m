4091810159422c47320daae8324047435c0d23c9
Franziska <franziska.boenisch.fb@gmail.com>
2018-11-21 11:51:15 +0100

    added loading for several more parameters

--- visualization/loadTrajectoryFilesFromFolder.m
+++ visualization/loadTrajectoryFilesFromFolder.m
@@ -4,10 +4,24 @@
 % - '*.ups': upsampled by spline interpolation
 function Params = loadTrajectoryFilesFromFolder(folder, fileending)
 
+% default we assume raw files
+if nargin < 2
+    fileending = '*.raw';
+end
+
 % transformation matrix
 trans_path = strcat(folder,'\video\Transform.mat');
+% for the matrix with the waggle indices
+i_path = strcat(folder,'\video\I.mat');
 Trans = importdata(trans_path);
 Params.Trans = Trans;
+
+% load the matrix that contains the start and stop indices for waggles
+I = importdata(i_path);
+Params.I = I;
+
+%framerate
+Params.framerate = 50; % for all robotic dances, framerate is 50
 
 % tracks
 folder = strcat(folder,'\trajectories');
@@ -15,6 +29,13 @@ D = dir(fullfile(folder, fileending));
 
 k = 1;
 for i = 1:length(D)
+    
+    % safe at what position the robot is 
+    if strncmpi(D(i).name,'followers',9) %followers has 9 letters
+         Params.id_flw = i;
+    else
+        Params.id_dancer = i;
+    end
     
     T          = loadTrack([folder '\' D(i).name]);
 
@@ -25,4 +46,22 @@ for i = 1:length(D)
     Params.T{k} = T(idx_nnan, :);
     k = k + 1;
     Params.fileending = fileending;
+    
+    Params.space_norm = 5; % HACK: 5 / 5 is 1 --> for robotic dances
+     
+%     this is an example of how we saved the I's to the folders
+%     I = [   232 250
+%             366 386
+%             500 520 
+%             634 653 
+%             769 788 
+%             904 922 
+%             1037 1056 
+%             1171 1190
+%             1306 1325
+%             1441 1459   ];
+% 
+%     I = 10*(I - Params.T{1}(1,1));
+%     
+%     save(i_path, 'I');
 end
