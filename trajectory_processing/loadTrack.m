85029411cc5cb118a5f217376191c2b75391d84b
Franziska <franziska.boenisch.fb@gmail.com>
2018-12-18 13:09:08 +0100

    added possibility to ignore header row when reading csv

--- trajectory_processing/loadTrack.m
+++ trajectory_processing/loadTrack.m
@@ -2,6 +2,7 @@
 function T = loadTrack(trackPath)
     % load track from file
     fid     = fopen(trackPath, 'r');
+    %fgetl(fid);  % Ignore first line header line
     R       = fscanf(fid, '%d %f %f %f %f %f');
     T       = reshape(R, 6, length(R)/6)';
     fclose(fid);
